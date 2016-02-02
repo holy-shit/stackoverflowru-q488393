ee5dbcc2e0b73cabe229a8a2a70a228e483ef20e
Oleg Chirukhin <olegchiruhin@gmail.com>
2016-02-02 20:15:34 +0600

    симуляция привередливого метода

--- src/main/java/com/olegchir/stackoverflowru/q488393/App.java
+++ src/main/java/com/olegchir/stackoverflowru/q488393/App.java
@@ -8,17 +8,31 @@ import org.apache.commons.beanutils.BeanUtils;
 public class App {
     public static void main(String[] args) {
 
+        //Готовим исходные данные
         B b = new B();
         b.setX("123");
-
         A a = new A();
 
+        //Копируем поля между классами
         try {
             BeanUtils.copyProperties(a, b);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        System.out.println(a.getX());
+        //Вот эта строчка должна вывести только плохое
+        test(b);
+
+        //А вот это уже хорошее!
+        test(a);
+
+    }
+
+    public static void test(A a) {
+        if (a.getClass().equals(A.class)) {
+            System.out.println(String.format("Все просто замечательно, и значение = %s", a.getX()));
+        } else {
+            System.out.println("Ты пытаешься выполнить меня с неверным классом!");
+        }
     }
 }
